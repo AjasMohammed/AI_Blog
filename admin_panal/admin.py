76a91933f878d745a34e3f749a9b9d8b62e1c4ef
Ajas Mohammed <ajasmohammed09@gmail.com>
2023-06-02 23:14:26 +0300

    moved the Post model from blog to adminPanal.Now blogpost contents can be seen in the page. created a page called blogpost to show the individual contents.

--- admin_panal/admin.py
+++ admin_panal/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from admin_panal.models import BlogPost
+from admin_panal.models import BlogUrl, Post
 
 # Register your models here.
-admin.site.register((BlogPost))
+admin.site.register((BlogUrl))
+admin.site.register((Post))
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from admin_panal.models import Post
+
 
 # Create your views here.
-def blogposts(request):
+def bloghome(request):
     
-    return render(request, 'blog/blog.html')
+    posts = Post.objects.all()
+    data = {'posts': posts}
+
+    return render(request, 'blog/blog.html', data)
+
+
+def blog_post(request, sno):
+    post = Post.objects.get(serial_no=sno)
+    context = {'post': post}
+    return render(request, "blog/blogpost.html", context)
+
+    
+
+
