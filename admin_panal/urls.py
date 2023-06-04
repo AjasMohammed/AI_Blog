1f87476d291f4943ee64e070d9efac9a68d6bb34
Ajas Mohammed <ajasmohammed09@gmail.com>
2023-06-04 23:15:44 +0300

    delete button in the adminpanal is now working. a function is created to delete items from the url database.

--- admin_panal/urls.py
+++ admin_panal/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.post_url, name='post_url')
+    path('', views.post_url, name='post_url'),
+    path('<int:serial_no>', views.delete_item, name="delete_item"),
 ]
