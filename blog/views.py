cdf0747beb1544422c0318a7dddf58f4b477618c
Ajas Mohammed <ajasmohammed09@gmail.com>
2023-06-06 12:34:45 +0300

    added pagination to the blog page, now only 5 posts will show in the page . if there are more than 5 it will be shown in the nextpage. django paginator is used to collect objects from the database and paginate it according to the requirments.

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from admin_panal.models import Post
+from django.core.paginator import Paginator
 
 
 # Create your views here.
 def bloghome(request):
     
     posts = Post.objects.all()
-    data = {'posts': posts}
+    
+    paginator = Paginator(posts, 5)  # Create a Paginator instance with 5 posts per page
+    page_no = request.GET.get('page')  # Get the current page number from the GET parameters
+    page_obj = paginator.get_page(page_no)  # Get the Page object for the current page
+    posts = page_obj.object_list  # Retrieve the posts for the current page
+
+    data = {'posts': posts, 'page_obj': page_obj}
 
     return render(request, 'blog/blog.html', data)
 
