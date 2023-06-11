319670fc3372136edbecdd7778a45d105df7f766
Ajas Mohammed <ajasmohammed09@gmail.com>
2023-06-11 18:08:29 +0300

    created a new python file to call bard AI . created a function in it which returns bard object. In tasks.py the bard object is imported from the file. The bard AI can be called from anywhere from the file without creating the object repeatedly.

---
+++ admin_panal/bard_ai.py
@@ -0,0 +1,8 @@
+from bardapi import Bard
+
+
+def call_bard():
+    token = "Wwgs-er8fHvJu14FiH1VvBN2Cp4a4MciTct25-JCQr5pBgxD1BMnI5zIfKG-JNT3yC2hHg."
+    bard = Bard(token=token)
+    
+    return bard
