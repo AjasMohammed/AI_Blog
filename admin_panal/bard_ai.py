44ed2007242d51e6ca972304ad0297e8fe3375a9
Ajas <ajasmohammed33@gmail.com>
2023-07-28 00:30:39 +0530

    **

--- admin_panal/bard_ai.py
+++ admin_panal/bard_ai.py
@@ -2,7 +2,7 @@ from bardapi import Bard
 
 
 def call_bard():
-    token = "Wwgs-er8fHvJu14FiH1VvBN2Cp4a4MciTct25-JCQr5pBgxD1BMnI5zIfKG-JNT3yC2hHg."
+    token = "__Secure-1PSID"
     bard = Bard(token=token)
     
     return bard
