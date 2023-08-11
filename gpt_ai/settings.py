4b260004d23ebbe4816689e89f6be0aec83f5c64
Ajas Mohammed <ajasmohammed33@gmail.com>
2023-08-11 22:59:16 +0530

    Created a .env file

--- gpt_ai/settings.py
+++ gpt_ai/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os.path
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3f66t$6rj3ti^*zxzb%msr%)0vyye1(l!vgkq%e2mia%&9va9d'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
